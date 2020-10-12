33648a6e6f1eafad20dd31e253e8f0a1781f9a8c
tomoya1218 <72542998+tomoya1218@users.noreply.github.com>
2020-10-12 11:46:08 +0900

    Add files via upload

---
+++ ConventionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Convention ViewController.swift
+//  driveRecord
+//
+//  Created by ともや n on 2020/10/12.
+//
+
+import UIKit
+
+class ConventionViewController : UIViewController {
+    
+}
