a075ccb43d0cf7bd7fddeb225d20b4aceb61af85
大越悠司 <yuji-okoshi@cimtech.co.jp>
2020-10-30 14:05:23 +0900

    アクセス修飾子とインデントの修正

--- driveRecord/MoneyInsertViewController.swift
+++ driveRecord/MoneyInsertViewController.swift
@@ -16,148 +16,148 @@ class MoneyInsertViewController : UIViewController, UIPickerViewDelegate, UIPick
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerView.tag == 1 {
-         return list.count
+            return list.count
         } else {
             return repayerList.count
         }
     }
     
     // UIPickerViewの最初の表示
-        func pickerView(_ pickerView: UIPickerView,
-                        titleForRow row: Int,
-                        forComponent component: Int) -> String? {
-            if pickerView.tag == 1 {
-                return list[row]
-            } else {
-                return repayerList[row]
-            }
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return list[row]
+        } else {
+            return repayerList[row]
         }
+    }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-            if pickerView.tag == 1 {      // <<<<<<<<<<　変更
-                List.text = list[row]
-            } else {
-                selectRepayer.text = repayerList[row]
-            }
+        if pickerView.tag == 1 {      // <<<<<<<<<<　変更
+            List.text = list[row]
+        } else {
+            selectRepayer.text = repayerList[row]
         }
-  
-
+    }
+    
+    
     //値の取得
     var receiveId:Int64 = 0
     //項目の変数
-    @IBOutlet weak var List: UITextField!
+    @IBOutlet private weak var List: UITextField!
     //金額入力を管理する変数
-    @IBOutlet weak var money: UITextField!
+    @IBOutlet private weak var money: UITextField!
     //負担者を選択する変数
-    @IBOutlet weak var selectRepayer: UITextField!
+    @IBOutlet private weak var selectRepayer: UITextField!
     
-    var pickerView: UIPickerView = UIPickerView()
-    var pickerView2: UIPickerView = UIPickerView()
+    private var pickerView: UIPickerView = UIPickerView()
+    private var pickerView2: UIPickerView = UIPickerView()
     //項目の選択肢
-    let list: [String] = ["レンタカー代", "ガソリン代", "高速道路代", "駐車場代", "その他"]
-    var member1:String?
-    var member2: String?
-    var member3: String?
-    var member4: String?
-    var member5: String?
-    var member6: String?
-        
+    private let list: [String] = ["レンタカー代", "ガソリン代", "高速道路代", "駐車場代", "その他"]
+    private var member1:String?
+    private var member2: String?
+    private var member3: String?
+    private var member4: String?
+    private var member5: String?
+    private var member6: String?
+    
     
     //負担者を格納する変数
     var repayerList:[String?] = []
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            //データベース接続
-            let helper = DatabaseHelper()
-            //前画面で作成したfolderinfoの一列情報を格納するための変数
-            var entity: Folderinfo?
-            //前画面のfolderid情報を基に検索を行う
-            let result2 = helper.inDatabase { (db) in
-                //全画面で登録したフォルダ情報を取得
-                entity = try Folderinfo.filter(Folderinfo.Columns.folderid == receiveId).fetchOne(db)
-            }
-                if(!result2) {
-                    print("sippai")
-                } else {
-                    //負担者のViewPickerを作成するため、検索結果から取り出す
-                     member1 = entity?.member1
-                     member2 = entity?.member2
-                     member3 = entity?.member3
-                     member4 = entity?.member4
-                     member5 = entity?.member5
-                     member6 = entity?.member6
-                    
-                    //入力されているか判定し、配列に格納をする
-                    if !(member1!.isEmpty) {
-                        repayerList.append(member1)
-                    }
-                    if !(member2!.isEmpty) {
-                        repayerList.append(member2)
-                    }
-                    if !(member3!.isEmpty) {
-                        repayerList.append(member3)
-                    }
-                    if !(member4!.isEmpty){
-                        repayerList.append(member4)
-                    }
-                    if !(member5!.isEmpty) {
-                        repayerList.append(member5)
-                    }
-                    if !(member5!.isEmpty) {
-                        repayerList.append(member6)
-                    }
-                }
-            
-            pickerView.tag = 1
-            pickerView2.tag = 2
-            // ピッカー設定
-            pickerView.delegate = self
-            pickerView.dataSource = self
-            pickerView2.delegate = self
-            pickerView2.dataSource = self
-            
-            
-            //決定バーの生成
-            let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
-            //let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-            //let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
-            //toolbar.setItems([spacelItem, doneItem], animated: true)
-            
-
-            // 左側のBarButtonItemはflexibleSpace。これがないと右に寄らない。
-            let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
-            // Doneボタン
-            let commitButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(commitButtonTapped))
-
-            // BarButtonItemの配置
-            toolbar.items = [spacer, commitButton]
-            // textViewのキーボードにツールバーを設定
-            money.inputAccessoryView = toolbar
-            selectRepayer.inputAccessoryView = toolbar
-            // インプットビュー設定
-            List.inputView = pickerView
-            List.inputAccessoryView = toolbar
-            selectRepayer.inputView = pickerView2
-            
-            //金額入力を数字のみにする
-            self.money.keyboardType = UIKeyboardType.numberPad
-            
-           
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //データベース接続
+        let helper = DatabaseHelper()
+        //前画面で作成したfolderinfoの一列情報を格納するための変数
+        var entity: Folderinfo?
+        //前画面のfolderid情報を基に検索を行う
+        let result2 = helper.inDatabase { (db) in
+            //全画面で登録したフォルダ情報を取得
+            entity = try Folderinfo.filter(Folderinfo.Columns.folderid == receiveId).fetchOne(db)
         }
+        if(!result2) {
+            print("sippai")
+        } else {
+            //負担者のViewPickerを作成するため、検索結果から取り出す
+            member1 = entity?.member1
+            member2 = entity?.member2
+            member3 = entity?.member3
+            member4 = entity?.member4
+            member5 = entity?.member5
+            member6 = entity?.member6
+            
+            //入力されているか判定し、配列に格納をする
+            if !(member1!.isEmpty) {
+                repayerList.append(member1)
+            }
+            if !(member2!.isEmpty) {
+                repayerList.append(member2)
+            }
+            if !(member3!.isEmpty) {
+                repayerList.append(member3)
+            }
+            if !(member4!.isEmpty){
+                repayerList.append(member4)
+            }
+            if !(member5!.isEmpty) {
+                repayerList.append(member5)
+            }
+            if !(member5!.isEmpty) {
+                repayerList.append(member6)
+            }
+        }
+        
+        pickerView.tag = 1
+        pickerView2.tag = 2
+        // ピッカー設定
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView2.delegate = self
+        pickerView2.dataSource = self
+        
+        
+        //決定バーの生成
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
+        //let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        //let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        //toolbar.setItems([spacelItem, doneItem], animated: true)
+        
+        
+        // 左側のBarButtonItemはflexibleSpace。これがないと右に寄らない。
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        // Doneボタン
+        let commitButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(commitButtonTapped))
+        
+        // BarButtonItemの配置
+        toolbar.items = [spacer, commitButton]
+        // textViewのキーボードにツールバーを設定
+        money.inputAccessoryView = toolbar
+        selectRepayer.inputAccessoryView = toolbar
+        // インプットビュー設定
+        List.inputView = pickerView
+        List.inputAccessoryView = toolbar
+        selectRepayer.inputView = pickerView2
+        
+        //金額入力を数字のみにする
+        self.money.keyboardType = UIKeyboardType.numberPad
+        
+        
+    }
     
     
     /*入力完了後、キーボードを閉じる
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            self.view.endEditing(true)
-        } */
-
+     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     self.view.endEditing(true)
+     } */
+    
     
     @objc func onClickCommitButton (sender: UIButton) {
-            if(money.isFirstResponder){
-                money.resignFirstResponder()
-            }
+        if(money.isFirstResponder){
+            money.resignFirstResponder()
         }
+    }
     
     @objc func commitButtonTapped() {
         self.view.endEditing(true)
@@ -165,17 +165,17 @@ class MoneyInsertViewController : UIViewController, UIPickerViewDelegate, UIPick
         
     }
     
- 
-     
+    
+    
     /*決定ボタン押下
-    @objc func done() {
-        List.endEditing(true)
-        List.text = "\(list[pickerView.selectedRow(inComponent: 0)])"
-    } */
+     @objc func done() {
+     List.endEditing(true)
+     List.text = "\(list[pickerView.selectedRow(inComponent: 0)])"
+     } */
     
     
     @IBAction func complete(_ sender: Any) {
-        //各TextFieldから値を取得し型を変換
+        //各TextFieldから値を取得し格納
         let selectList:String! = List.text
         let inputMoney:String! = money.text
         //StringからInt型への変換
@@ -196,86 +196,86 @@ class MoneyInsertViewController : UIViewController, UIPickerViewDelegate, UIPick
             present(alert, animated: true, completion: nil)
         } else {
             
-        let helper2 = DatabaseHelper()
-        let result = helper2.inDatabase{(db) in
-            //receivedIdは送信されたフォルダ番号
-            let folder = Paragraphinfo(folderid: receiveId, para_name: selectList, para_cost: intMoney, repayer: chooseRepayer)
-            //登録処理
-            try folder.insert(db)
-        }
-        if (!result) {
-            print("失敗")
-        } else {
-            print("成功")
-            //登録済みか確認用コード
-            var result3 :[Paragraphinfo] = []
-            let helper4 = DatabaseHelper()
-
-            let str = helper4.inDatabase { (db) in
-                result3 = try Paragraphinfo.fetchAll(db)
+            let helper2 = DatabaseHelper()
+            let result = helper2.inDatabase{(db) in
+                //receivedIdは送信されたフォルダ番号
+                let folder = Paragraphinfo(folderid: receiveId, para_name: selectList, para_cost: intMoney, repayer: chooseRepayer)
+                //登録処理
+                try folder.insert(db)
             }
-            // 出力
-            result3.forEach { (it) in
-                print("\(String(describing: it.folderid)) - \(String(describing: it.para_name)) - \(String(describing: it.para_cost)) - \(String(describing: it.para_num))")
-            }
-            if (!str) {
+            if (!result) {
                 print("失敗")
             } else {
-            
+                print("成功")
+                //登録済みか確認用コード
+                var result3 :[Paragraphinfo] = []
+                let helper4 = DatabaseHelper()
+                
+                let str = helper4.inDatabase { (db) in
+                    result3 = try Paragraphinfo.fetchAll(db)
+                }
+                // 出力
+                result3.forEach { (it) in
+                    print("\(String(describing: it.folderid)) - \(String(describing: it.para_name)) - \(String(describing: it.para_cost)) - \(String(describing: it.para_num))")
+                }
+                if (!str) {
+                    print("失敗")
+                } else {
+                    
+                }
+                
             }
+            
+            let alert: UIAlertController = UIAlertController( title: "", message: "どちらに移動しますか？", preferredStyle:  UIAlertController.Style.alert)
+            
+            // ② Actionの設定
+            // Action初期化時にタイトル, スタイル, 押された時に実行されるハンドラを指定する
+            // 第3引数のUIAlertActionStyleでボタンのスタイルを指定する
+            // 詳細ボタン
+            let detailAction: UIAlertAction = UIAlertAction(title: "詳細", style: UIAlertAction.Style.default, handler:{
+                // ボタンが押された時の処理を書く（クロージャ実装）
+                (action: UIAlertAction!) -> Void in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.performSegue(withIdentifier: "detail", sender: nil)
+                    /* storyboardのインスタンス取得
+                     let storyboard1: UIStoryboard = self.storyboard!
+                     
+                     // 遷移先ViewControllerのインスタンス取得
+                     let nextView1 = storyboard1.instantiateViewController(withIdentifier:"detail")
+                     
+                     // 画面遷移
+                     self.present(nextView1, animated: true, completion: nil)
+                     }*/
+                    print("詳細")
+                }
+            }
+            )
+            // ホームボタン
+            let homeAction: UIAlertAction = UIAlertAction(title: "ホーム", style: UIAlertAction.Style.default, handler:{
+                // ボタンが押された時の処理を書く（クロージャ実装）
+                (action: UIAlertAction!) -> Void in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    // storyboardのインスタンス取得
+                    let storyboard2: UIStoryboard = self.storyboard!
+                    
+                    // 遷移先ViewControllerのインスタンス取得
+                    let nextView2 = storyboard2.instantiateViewController(withIdentifier: "home")
+                    
+                    // 画面遷移
+                    self.present(nextView2, animated: true, completion: nil)
+                }
+                print("ホーム")
+            }
+            )
+            
+            // ③ UIAlertControllerにActionを追加
+            alert.addAction(homeAction)
+            alert.addAction(detailAction)
+            // ④ Alertを表示
+            present(alert, animated: true, completion: nil)
             
         }
-        
-        let alert: UIAlertController = UIAlertController( title: "", message: "どちらに移動しますか？", preferredStyle:  UIAlertController.Style.alert)
-
-        // ② Actionの設定
-        // Action初期化時にタイトル, スタイル, 押された時に実行されるハンドラを指定する
-        // 第3引数のUIAlertActionStyleでボタンのスタイルを指定する
-        // 詳細ボタン
-            let detailAction: UIAlertAction = UIAlertAction(title: "詳細", style: UIAlertAction.Style.default, handler:{
-            // ボタンが押された時の処理を書く（クロージャ実装）
-            (action: UIAlertAction!) -> Void in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                self.performSegue(withIdentifier: "detail", sender: nil)
-                /* storyboardのインスタンス取得
-                        let storyboard1: UIStoryboard = self.storyboard!
-                 
-                        // 遷移先ViewControllerのインスタンス取得
-                        let nextView1 = storyboard1.instantiateViewController(withIdentifier:"detail")
-                
-                        // 画面遷移
-                        self.present(nextView1, animated: true, completion: nil)
-                            }*/
-            print("詳細")
-            }
-            }
-    )
-        // ホームボタン
-        let homeAction: UIAlertAction = UIAlertAction(title: "ホーム", style: UIAlertAction.Style.default, handler:{
-            // ボタンが押された時の処理を書く（クロージャ実装）
-            (action: UIAlertAction!) -> Void in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                // storyboardのインスタンス取得
-                        let storyboard2: UIStoryboard = self.storyboard!
-                 
-                        // 遷移先ViewControllerのインスタンス取得
-                let nextView2 = storyboard2.instantiateViewController(withIdentifier: "home")
-                
-                        // 画面遷移
-                        self.present(nextView2, animated: true, completion: nil)
-                            }
-            print("ホーム")
-         }
-    )
-
-        // ③ UIAlertControllerにActionを追加
-        alert.addAction(homeAction)
-        alert.addAction(detailAction)
-        // ④ Alertを表示
-        present(alert, animated: true, completion: nil)
-
     }
 }
-}
-            
+
 
--- driveRecord/folderCreateViewController.swift
+++ driveRecord/folderCreateViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import GRDB
 
 class FolderCreateViewController : UIViewController, UITextFieldDelegate {
-    //テスト
+    
     //日付のTextField
     @IBOutlet weak var dateTravel: UITextField!
     //タイトル名のTextField
@@ -33,7 +33,7 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
     
     @IBAction func mem1(_ sender: Any) {
         if let text = member1.text, !text.isEmpty {
-           count += 1
+            count += 1
         } else {
             count -= 1
         }
@@ -43,16 +43,16 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
     
     @IBAction func mem2(_ sender: Any) {
         if let text = member2.text, !text.isEmpty {
-           count += 1
+            count += 1
         } else {
             count -= 1
         }
         countMember.text = "人数：\(count)人"
     }
-   
+    
     @IBAction func mem3(_ sender: Any) {
         if let text = member3.text, !text.isEmpty {
-           count += 1
+            count += 1
         } else {
             count -= 1
         }
@@ -61,7 +61,7 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
     
     @IBAction func mem4(_ sender: Any) {
         if let text = member4.text, !text.isEmpty {
-           count += 1
+            count += 1
         } else {
             count -= 1
         }
@@ -70,7 +70,7 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
     
     @IBAction func mem5(_ sender: Any) {
         if let text = member5.text, !text.isEmpty {
-           count += 1
+            count += 1
         } else {
             count -= 1
         }
@@ -79,7 +79,7 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
     
     @IBAction func mem6(_ sender: Any) {
         if let text = member6.text, !text.isEmpty {
-           count += 1
+            count += 1
         } else {
             count -= 1
         }
@@ -88,61 +88,61 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
     
     
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            //OSの変更
-            if #available(iOS 13.4, *) {
-                datePicker.preferredDatePickerStyle = .wheels
-            }
-            dateTravel.inputView = datePicker
-            
-            //UserDefaultのインスタンス生成
-            //let setting = UserDefaults.standard
-            //UserDefaultに初期値を設定
-            //setting.register(defaults: [settingKey:0])
-            //キーボード処理を終了するための処理
-            travelTitle.delegate = self
-            member1.delegate = self
-            member2.delegate = self
-            member3.delegate = self
-            member4.delegate = self
-            member5.delegate = self
-            member6.delegate = self
-            
-            //displayUpdate()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //OSの変更
+        if #available(iOS 13.4, *) {
+            datePicker.preferredDatePickerStyle = .wheels
         }
+        dateTravel.inputView = datePicker
         
+        //UserDefaultのインスタンス生成
+        //let setting = UserDefaults.standard
+        //UserDefaultに初期値を設定
+        //setting.register(defaults: [settingKey:0])
+        //キーボード処理を終了するための処理
+        travelTitle.delegate = self
+        member1.delegate = self
+        member2.delegate = self
+        member3.delegate = self
+        member4.delegate = self
+        member5.delegate = self
+        member6.delegate = self
+        
+        //displayUpdate()
+    }
+    
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-     
-            //label.text = textField.text
-            // キーボードを閉じる
-            textField.resignFirstResponder()
-            //キーボード処理終了時に、カウントを行う
-            //displayUpdate()
-     
-            return true
-        }
-     
+        
+        //label.text = textField.text
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        //キーボード処理終了時に、カウントを行う
+        //displayUpdate()
+        
+        return true
+    }
+    
     
     let datePicker: UIDatePicker = {
-            
-                let dp = UIDatePicker()
-                dp.datePickerMode = UIDatePicker.Mode.date
-                dp.timeZone = NSTimeZone.local
-                dp.locale = Locale.current
-                dp.addTarget(self, action: #selector(dateChange), for: .valueChanged)
-                return dp
-            }()
-        @objc func dateChange(){
-            
-                let formatter = DateFormatter()
-                formatter.dateFormat = "MM/dd"
-                dateTravel.text = "\(formatter.string(from: datePicker.date))"
-            }
-
-   
+        
+        let dp = UIDatePicker()
+        dp.datePickerMode = UIDatePicker.Mode.date
+        dp.timeZone = NSTimeZone.local
+        dp.locale = Locale.current
+        dp.addTarget(self, action: #selector(dateChange), for: .valueChanged)
+        return dp
+    }()
+    @objc func dateChange(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd"
+        dateTravel.text = "\(formatter.string(from: datePicker.date))"
+    }
+    
+    
     //金額入力ボタン押下後
     @IBAction func Insert(_ sender: UIButton) {
         //各TextFieldから値を取得
@@ -176,12 +176,12 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
                                           preferredStyle: UIAlertController.Style.alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
-        
+            
         } else {
             //データベース接続
             //let helper = DatabaseHelper()
             let result = helper.inDatabase{(db) in
-            let folder = Folderinfo(title: travelNameString, date: dateTravelDate, member1: travelMember1String, member2:travelMember2String,member3: travelMember3String,member4: travelMember4String,member5: travelMember5String,member6: travelMember6String)
+                let folder = Folderinfo(title: travelNameString, date: dateTravelDate, member1: travelMember1String, member2:travelMember2String,member3: travelMember3String,member4: travelMember4String,member5: travelMember5String,member6: travelMember6String)
                 //登録処理
                 try folder.insert(db)
             }
@@ -209,21 +209,21 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
                     //Segueの実行
                     performSegue(withIdentifier: "moneyView", sender: nil)
                     
-                  
+                    
                     /*storyboardのインスタンス取得
-                    let storyboard: UIStoryboard = self.storyboard!
-                    //遷移先のインスタンスを取得
-                    let nextView = storyboard.instantiateViewController(withIdentifier: "moneyView") as! MoneyInsertViewController
-                    //値の設定
-                    nextView.receiveId = sendId!
-                    //画面遷移
-                    self.present(nextView, animated: true, completion: nil)*/
+                     let storyboard: UIStoryboard = self.storyboard!
+                     //遷移先のインスタンスを取得
+                     let nextView = storyboard.instantiateViewController(withIdentifier: "moneyView") as! MoneyInsertViewController
+                     //値の設定
+                     nextView.receiveId = sendId!
+                     //画面遷移
+                     self.present(nextView, animated: true, completion: nil)*/
                     
                 }
             }
-        
+            
         }
-}
+    }
     //Segue実行前の処理
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //Segueの識別子確認
@@ -237,7 +237,7 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
             nextView.receiveId = sendId!
         }
     }
-   //作成完了ボタン押下時の処理
+    //作成完了ボタン押下時の処理
     @IBAction func InsertHome(_ sender: UIButton) {
         //各TextFieldから値を取得
         let dateTravelDate: String! = dateTravel.text
@@ -268,13 +268,13 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
                                           preferredStyle: UIAlertController.Style.alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
-        
+            
         } else {
-    
+            
             //データベース接続
             let helper = DatabaseHelper()
             let result = helper.inDatabase{(db) in
-            let folder = Folderinfo(title: travelNameString, date: dateTravelDate, member1: travelMember1String, member2:travelMember2String,member3: travelMember3String,member4: travelMember4String,member5: travelMember5String,member6: travelMember6String)
+                let folder = Folderinfo(title: travelNameString, date: dateTravelDate, member1: travelMember1String, member2:travelMember2String,member3: travelMember3String,member4: travelMember4String,member5: travelMember5String,member6: travelMember6String)
                 //登録処理
                 try folder.insert(db)
             }
@@ -288,8 +288,8 @@ class FolderCreateViewController : UIViewController, UITextFieldDelegate {
                 print(travelMember1String!)
                 print(travelMember6String!)
             }
-           
-        
+            
+            
         }
     }
 }
