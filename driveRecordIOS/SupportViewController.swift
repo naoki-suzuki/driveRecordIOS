dd292672f01f62a206bc3df802acc02bbf0ad7e5
大越悠司 <yuji-okoshi@cimtech.co.jp>
2020-11-18 09:06:43 +0900

    ライセンス表示の処理

--- driveRecordIOS/SupportViewController.swift
+++ driveRecordIOS/SupportViewController.swift
@@ -3,10 +3,22 @@
 //  driveRecord
 //
 //  Created by 大越悠司 on 2020/10/12.
-//
+//  Update by 大越悠司　on 2020/11/17
 
 import UIKit
 
 class SupportViewController : UIViewController {
+    
+    // ライセンスボタンを押下時の処理
+    @IBAction func licens(_ sender: Any) {
+        showOSSettingView()
+    }
+    
+    // 設定画面に遷移を行うメソッド
+    func showOSSettingView() {
+        if let url = URL(string: UIApplication.openSettingsURLString) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
     
 }
