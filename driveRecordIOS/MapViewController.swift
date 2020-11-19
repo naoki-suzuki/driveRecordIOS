188cb642d6850e084a8832b1cd96e359e6f6c93f
鈴木直輝 <>
2020-11-19 11:19:04 +0900

    地図の初期画面の不具合修正（初期画面を東京駅に設定）

--- driveRecordIOS/MapViewController.swift
+++ driveRecordIOS/MapViewController.swift
@@ -21,68 +21,63 @@ import GoogleMaps
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
-    // 
+    // 現在地
     var locationManager = CLLocationManager()
     
     //
     var mapView = GMSMapView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-        //初期値はApple本社
-        let camera = GMSCameraPosition.camera(withLatitude: 37.3318, longitude: -122.0312, zoom: 17.0)
-        mapView = GMSMapView.map(withFrame: CGRect(origin: .zero, size: view.bounds.size), camera: camera)
+        setupMap()
+        requestLoacion() //
+    }
+    
+    private func setupMap() {
+        
+        // GoogleMapの初期位置(仮で東京駅付近に設定)
+        let camera = GMSCameraPosition.camera(withLatitude: 35.6812226, longitude: 139.7670594, zoom: 12.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
         mapView.settings.myLocationButton = true //右下のボタン追加する
-        mapView.isMyLocationEnabled = true
-
-        // User Location
-        locationManager.delegate = self
+        mapView.isMyLocationEnabled = true //現在地
+        view = mapView
+    }
+    
+    // 現在地
+    private func requestLoacion() {
+        // ユーザにアプリ使用中のみ位置情報取得の許可を求めるダイアログを表示
         locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
-
-        self.view.addSubview(mapView)
-        self.view.bringSubviewToFront(mapView)
-    }
-
-    //現在地が更新されたら呼び出される
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let userLocation = locations.last
-
-        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
-                                                          longitude: userLocation!.coordinate.latitude, zoom: 17.0)
-        self.mapView.animate(to: camera)
-
-        locationManager.stopUpdatingLocation()
+        // 常に取得したい場合はこちら↓
+        // locationManager.requestAlwaysAuthorization()
     }
     
+    func mapView(mapView: GMSMapView!, didLongPressAtCoordinate coordinate: CLLocationCoordinate2D) {
+        print("Tapped at coordinate: " + String(coordinate.latitude) + " "
+                + String(coordinate.longitude))
+    }
     
-//    // マーカーのタップを検知するメソッド
-//    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
-//        return true
-//    }
     
     /**
-      マップにマーカを設置する処理
-
-      - parameter title: マーカのタイトル
-      - parameter coordinate: 位置
-      - parameter iconName: アイコン名
-      - parameter completion: Callback
-    */
+     マップにマーカを設置する処理
+     - parameter title: マーカのタイトル
+     - parameter coordinate: 位置
+     - parameter iconName: アイコン名
+     - parameter completion: Callback
+     */
     private func putMarker(title: String?, coordinate: CLLocationCoordinate2D, iconName: String?, completion: @escaping ((GMSMarker) -> Void)) {
-      // マーカの生成
-      let marker = GMSMarker()
-      marker.title = title
-      marker.position = coordinate
-      if iconName != nil {
-        // アイコン名が指定されている場合は画像を設定
-        marker.icon = UIImage.init(named: iconName!)
-      }
-      marker.map = self.mapView
-      completion(marker)
+        // マーカの生成
+        let marker = GMSMarker()
+        marker.title = title
+        marker.position = coordinate
+        if iconName != nil {
+            // アイコン名が指定されている場合は画像を設定
+            marker.icon = UIImage.init(named: iconName!)
+        }
+        marker.map = self.mapView
+        completion(marker)
     }
+    
 }
 
