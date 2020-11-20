b693b33e8144dd2f40a7914e93072e10037426ec
大越悠司 <yuji-okoshi@cimtech.co.jp>
2020-11-20 14:13:42 +0900

    カメラの保存処理のコード変更 →保存するかどうかのアラート確認 　設定で写真の追加許可をしていない場合、エラーをアラートで表示
    MoneyInsert画面のレイアウト修正
    →写真保存画面のTopスペースの追加
    　金額入力などのTextfieldの幅を変更

--- driveRecordIOS/cameraViewController.swift
+++ driveRecordIOS/cameraViewController.swift
@@ -11,39 +11,64 @@ import UIKit
 class cameraViewController: UIViewController, UIImagePickerControllerDelegate,
                       UINavigationControllerDelegate{
     
-   
+    // 撮影した写真の表示
     @IBOutlet weak var cameraPic: UIImageView!
+    
+    // MoneyInsertで冊絵した写真を受け取る変数
     var cameraReceive: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // cameraPic.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         self.cameraPic.image = self.cameraReceive
+        
+        
     }
     
-   
+    
     // 撮影した写真の保存
     @IBAction func save(_ sender: Any) {
-   
+        // UIImageViewからUIImageへ型の変更
         let image:UIImage! = cameraPic.image
         
-        if image != nil {
-            UIImageWriteToSavedPhotosAlbum(
-                image,
-                self,
-                #selector(cameraViewController.image(_:didFinishSavingWithError:contextInfo:)),
-                nil)
-            
-            let alert = UIAlertController(title: "", message: "写真を保存しました" ,preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default,handler: nil))
-            present(alert, animated: true, completion: nil)
+        // 保存するか否かのアラート
+        let alertController = UIAlertController(title: "保存", message: "この画像を保存しますか？", preferredStyle: .alert)
+        // OK
+        let okAction = UIAlertAction(title: "OK", style: .default) { (ok) in
+            //ここでフォトライブラリに画像を保存
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
         }
-        else{
-            
+        // キャンセル
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .default) { (cancel) in
+            alertController.dismiss(animated: true, completion: nil)
         }
+        //　OKとキャンセルを表示追加し、アラートを表示
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
         
     }
-   
+    
+    // 保存結果をアラート
+    @objc func showResultOfSaveImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+        var title = "保存完了"
+        var message = "カメラロールに保存しました"
+        
+        if error != nil {
+            //保存許可を認めていない時、発生
+            title = "エラー"
+            message = "保存に失敗しました"
+        }
+        // アラート表示するタイトルとメッセージの格納
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // OKボタンを追加
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        // UIAlertController を表示
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     
     // 書き込み完了結果の受け取り
     @objc func image(_ image: UIImage,
