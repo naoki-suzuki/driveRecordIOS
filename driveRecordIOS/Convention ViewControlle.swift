206336b0ded2aca3fd403247e463c8442bad11f3
tomoya1218 <tomoya-nagasaka@cimtech.co.jp>
2020-12-28 17:12:12 +0900

    利用規約作成

--- driveRecordIOS/Convention ViewControlle.swift
+++ driveRecordIOS/Convention ViewControlle.swift
@@ -6,8 +6,24 @@
 //
 
 import UIKit
+import SafariServices
+
 
 class ConventionViewController : UIViewController {
     
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+    @IBAction func safariPrivacy(_ sender: Any) {
+        
+        let webPage = "https://www.cimtech.co.jp/privacy/"
+                let safariVC = SFSafariViewController(url: NSURL(string: webPage)! as URL)
+                safariVC.modalPresentationStyle = .pageSheet  // フルスクリーンにしたい場合はこの行を削除
+                present(safariVC, animated: true, completion: nil)
+    }
     
 }
