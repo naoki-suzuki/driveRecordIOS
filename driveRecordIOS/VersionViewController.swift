e9cddda5434c0396db98f53203aeb43514a1eb30
tomoya1218 <tomoya-nagasaka@cimtech.co.jp>
2021-02-03 15:29:56 +0900

    サポート画面の情報の追加

---
+++ driveRecordIOS/VersionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Version.swift
+//  driveRecordIOS
+//
+//  Created by ともや n on 2021/02/03.
+//
+
+import UIKit
+class VersionViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // ナビゲーションバーのアイテムの色　（戻る　＜　とか　読み込みゲージとか）
+        self.navigationController?.navigationBar.tintColor = .white
+        
+    
+    }
+}
